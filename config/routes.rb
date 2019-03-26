ac0e00ad29ca58446b185e950d98dadff5e00ba0
GuilhermeAroxa <guilherme.aroxa@gmail.com>
2019-03-26 20:46:03 -0300

    added categories crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories, except: :edit
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
