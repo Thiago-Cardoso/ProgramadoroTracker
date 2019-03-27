5f995aa1d67f7ef1669aacdeb6adeaee8fcee0bb
Thiago-Cardoso <trovador182@gmail.com>
2019-03-26 22:55:04 -0300

    merge stats and categories

--- db/migrate/20190326231222_create_categories.rb
+++ db/migrate/20190326231222_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :name
-
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_141151) do
+ActiveRecord::Schema.define(version: 2019_03_26_231222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "configuration_tasks", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.bigint "user_id"
-    t.integer "duration"
-    t.integer "duration_break"
-    t.integer "duration_break_long"
-    t.integer "long_pause_delay"
-    t.boolean "have_song"
-    t.integer "template"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_configuration_tasks_on_user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,5 +36,5 @@ ActiveRecord::Schema.define(version: 2019_03_26_141151) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "configuration_tasks", "users"
+  add_foreign_key "categories", "users"
 end
