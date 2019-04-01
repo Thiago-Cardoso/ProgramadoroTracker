e091c30f94322c0216b6932b38a05a24aba1f04a
Tiago Leal <tiago.sleal12@gmail.com>
2019-03-31 21:58:58 -0300

    create a capybara test

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Categories", type: :feature, js: true do
+RSpec.feature "Feature Categories", type: :feature, js: true do
   before do 
     @user = create(:user)
     login_as(@user, :scope => :user)
@@ -17,25 +17,5 @@ RSpec.feature "Categories", type: :feature, js: true do
     click_button("Enviar")
     expect(page).to have_content("Categoria cadastrada com sucesso!.")
   end
-
-  # it '#update' do
-  #   visit(new_category_path)
-  #   fill_in('category[description]', with: Faker::Name.name)
-  #   click_button("Enviar")
-
-
-  #   visit(edit_category_path)
-  #   fill_in('category[description]', with: Faker::Name.name)
-  #   click_button("Enviar")
-  #   expect(page).to have_content("Categoria cadastrada com sucesso!.")
-  # end
-
-  # it 'click button delete' do
-  #   visit(categories_path)
-  #   # find('.deletar').click_link('Deletar')
-  #   expect(page).to have_css('.deletar')
-  #   # expect(find("#my-div").find("h1")).to have_content("Yes!")
-  #   # expect(page).to have_content("Você tem Certeza?")
-  # end
 
 end
--- spec/features/configuration_tasks_spec.rb
+++ spec/features/configuration_tasks_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ConfigurationTasks", type: :feature, js: true do
+RSpec.feature "Feature ConfigurationTasks", type: :feature, js: true do
   before do 
     @user = create(:user)
     login_as(@user, :scope => :user)
@@ -11,27 +11,18 @@ RSpec.feature "ConfigurationTasks", type: :feature, js: true do
   #   expect(page).to have_current_path(configuration_task_path)
   # end
 
-  it '#create' do
-    visit(new_configuration_task_path)
-    # fill_in('template', with: "1")
-    # select('template', from: 'default 25 5 15 4')
-
-    # find('.template').all(:css, 'option').find { |o| o.value == '1' }.select_option
-    # page.select("1", from: 'template')
-
-    # page.select 'default 25 5 15 4', from: 'template'
-
-    # select('default 25 5 15 4', :from => 'template', visible: false)
-
-
-    fill_in('configuration_task[duration]', with: "25")
-    fill_in('configuration_task[duration_break]', with: "5")
-    fill_in('configuration_task[duration_break_long]', with: "15")
-    fill_in('configuration_task[long_pause_delay]', with: "4")
-    check('configuration_task[have_song]')
-    click_button("Cadastrar")
-    expect(page).to have_content("Configuração realizada com sucesso!.")
-  end
+  # it '#create' do
+    # visit(new_configuration_task_path)
+    # select('1', :from => 'template', :visible => 'false')
+    # select '1', from: 'Template'
+    # fill_in('configuration_task_duration', with: "25")
+    # fill_in('configuration_task[duration_break]', with: "5")
+    # fill_in('configuration_task[duration_break_long]', with: "15")
+    # fill_in('configuration_task[long_pause_delay]', with: "4")
+    # check('configuration_task[have_song]')
+    # click_button("Cadastrar")
+    # expect(page).to have_content("Configuração realizada com sucesso!.")
+  # end
 
 
 end
