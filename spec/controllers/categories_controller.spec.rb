a3b127abb6e5b130a654b80e3898b4718817eae3
Moacir Petry <m.petry9@gmail.com>
2019-04-20 20:36:25 -0300

    DRY spec/controllers/categories - get :index

--- spec/controllers/categories_controller.spec.rb
+++ spec/controllers/categories_controller.spec.rb
@@ -8,29 +8,27 @@ before(:each) do
   @request.env["devise.mapping"] = Devise.mappings[:user] #mapping devise
   @current_user = FactoryBot.create(:user)
   @categories = FactoryBot.create(:category)
-  sign_in @current_user   
+  sign_in @current_user
   end
   # is permited access devise index authenticated
-   describe "as a Guest" do
-      context "#index" do
-       it "responds successfully" do
-          @current_user   
-          get :index
-          expect(response).to be_success
-          end
-      end 
-
-       it "responds a 200 response" do
-          @current_user   
-          get :index
-          expect(response).to have_http_status(200)
-        end
+  describe "as a Guest" do
+    context "#index" do
+      subject { get :index }
+      it "responds successfully" do
+        @current_user
+        expect(subject).to be_success
+      end
+      it "responds a 200 response" do
+        @current_user
+        expect(subject).to have_http_status(200)
+      end
     end
+  end
 
      describe "as Logged user" do
-        it "#show" do 
+        it "#show" do
           #it is necessary authenticated in devise
-          @current_user   
+          @current_user
           get :show, params: {id: @categories.id}
           expect(response).to render_template(:show)
         end
@@ -45,7 +43,7 @@ before(:each) do
           @categories = attributes_for(:category, user: @current_user )
           post :create, params: {category: @categories}
         end
-    
+
         it "Redirect to new categories" do
           expect(response).to have_http_status(302)
           expect(response).to redirect_to("/categories/#{Category.last.id}")
@@ -57,7 +55,7 @@ before(:each) do
         before(:each) do
           request.env["HTTP_ACCEPT"] = 'application/json' #set all with json
         end
-   
+
         context "User is of the category" do
           it "returns http success" do
             category = create(:category, user: @current_user) #create category
@@ -72,15 +70,14 @@ before(:each) do
           @new_category_attributes = attributes_for(:category)
           request.env["HTTP_ACCEPT"] = 'application/json'
         end
-    
+
         context "updates the requested categories" do
           before(:each) do
             category = create(:category, user: @current_user)
             put :update, params: {id: category.id, category: @new_category_attributes}
             expect(response).to have_http_status(:success) #redirect
           end
-        end      
+        end
       end
 
 end
-    
